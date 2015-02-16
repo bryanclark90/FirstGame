7d8253e05b42decda3ec6df947ccffe997245e78
Bryan Clark <bryan.clark@email.wsu.edu>
2015-02-15 20:13:33 -0800

    Files created by libGDX

---
+++ SRC/html/src/com/myFirstGame/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.myFirstGame.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.myFirstGame.game.FirstGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new FirstGame();
+        }
+}
